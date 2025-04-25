45b570e5c624e00aec4cace7f1fabf559aeabd38
intel <76413417+syncintellect@users.noreply.github.com>
2025-04-25 18:39:01 -0500

    Add files via upload

---
+++ EaglerPackageLoader.js
@@ -0,0 +1,41 @@
+
+        "use strict";
+        (function(){
+            window.eaglercraftXOpts.assetsURI = "";
+
+            var launchInterval = -1;
+            var launchCounter = 1;
+            var launchCountdownNumberElement = null;
+            var launchCountdownProgressElement = null;
+            var launchSkipCountdown = false;
+
+            var launchTick = function() {
+                launchCountdownNumberElement.innerText = "" + Math.floor(6.0 - launchCounter * 0.06);
+                launchCountdownProgressElement.style.width = "" + launchCounter + "%";
+                if(++launchCounter > 100 || launchSkipCountdown) {
+                    clearInterval(launchInterval);
+                    setTimeout(function() {
+                        document.body.removeChild(document.getElementById("launch_countdown_screen"));
+                        document.body.style.backgroundColor = "black";
+                        main();
+                    }, 50);
+                }
+            };
+
+            window.addEventListener("load", function() {
+                launchCountdownNumberElement = document.getElementById("launchCountdownNumber");
+                launchCountdownProgressElement = document.getElementById("launchCountdownProgress");
+                launchInterval = setInterval(launchTick, 50);
+                document.getElementById("skipCountdown").addEventListener("click", function() {
+                    launchSkipCountdown = true;
+                });
+                document.getElementById("skipCountdown").focus();
+                /*
+                document.getElementById("bootMenu").addEventListener("click", function() {
+                    launchSkipCountdown = true;
+                    window.eaglercraftXOpts.showBootMenuOnLaunch = true;
+                });
+                */
+            });
+        })();
+    
---
+++ LaunchOptions.js
@@ -0,0 +1,13 @@
+
+        "use strict";
+        var relayId = Math.floor(Math.random() * 3);
+
+        // %%%%%%%%% launch options %%%%%%%%%%%%
+
+        window.eaglercraftXOpts = {
+            container: "game_frame",
+            worldsDB: "worlds"
+        };
+
+        // %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+    
